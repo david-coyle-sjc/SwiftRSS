39bfaf9cfa0283ce91bb5d2f5a202ca6f2dbbb22
David A Coyle <david.coyle@move.com>
2016-03-18 17:14:28 -0700

    Updates

--- SwiftRSS/RSSItem.swift
+++ SwiftRSS/RSSItem.swift
@@ -1,84 +1,87 @@
-//
-//  RSSItem.swift
-//  SwiftRSS_Example
-//
-//  Created by Thibaut LE LEVIER on 28/09/2014.
-//  Copyright (c) 2014 Thibaut LE LEVIER. All rights reserved.
-//
+//  Adapted from an example by Thibaut LE LEVIER on 05/09/2014.
 
 import UIKit
 
 class RSSItem: NSObject, NSCoding {
     var title: String?
-    var link: NSURL?
-    
-    func setLink1(let linkString: String!)
+    var links: [NSURL] = []
+    var propertyIDs: [String] = []
+
+    func addLink(linkString: String?)
     {
-        link = NSURL(string: linkString)
+        if let _ = linkString, let l = NSURL(string: linkString!) {
+            links.append(l)
+        }
     }
-    
+    func addPropertyID(propertyID: String?)
+    {
+        if let _ = propertyID {
+            propertyIDs.append(propertyID!)
+        }
+    }
+
     var guid: String?
     var pubDate: NSDate?
-    
+
     func setPubDate1(let dateString: String!)
     {
         pubDate = NSDate.dateFromInternetDateTimeString(dateString)
     }
-    
+
     var itemDescription: String?
     var content: String?
-    
+
     // Wordpress specifics
     var commentsLink: NSURL?
-    
+
     func setCommentsLink1(let linkString: String!)
     {
         commentsLink = NSURL(string: linkString)
     }
-    
+
     var commentsCount: Int?
-    
+
     var commentRSSLink: NSURL?
-    
+
     func setCommentRSSLink1(let linkString: String!)
     {
         commentRSSLink = NSURL(string: linkString)
     }
-    
+
     var author: String?
-    
+
     var categories: [String]! = [String]()
-    
+
     var imagesFromItemDescription: [NSURL]! {
         if let itemDescription = self.itemDescription
         {
             return itemDescription.imageLinksFromHTMLString
         }
-        
+
         return [NSURL]()
     }
-    
+
     var imagesFromContent: [NSURL]! {
         if let content = self.content
         {
             return content.imageLinksFromHTMLString
         }
-        
+
         return [NSURL]()
     }
-    
+
     override init()
     {
         super.init()
     }
-    
+
     // MARK: NSCoding
     required init?(coder aDecoder: NSCoder)
     {
         super.init()
-        
+
         title = aDecoder.decodeObjectForKey("title") as? String
-        link = aDecoder.decodeObjectForKey("link") as? NSURL
+        //        links = aDecoder.decodeObjectForKey("links") as? NSURL
         guid = aDecoder.decodeObjectForKey("guid") as? String
         pubDate = aDecoder.decodeObjectForKey("pubDate") as? NSDate
         itemDescription = aDecoder.decodeObjectForKey("description") as? String
@@ -89,59 +92,59 @@ class RSSItem: NSObject, NSCoding {
         author = aDecoder.decodeObjectForKey("author") as? String
         categories = aDecoder.decodeObjectForKey("categories") as? [String]
     }
-    
+
     func encodeWithCoder(aCoder: NSCoder)
     {
         if let title = self.title
         {
             aCoder.encodeObject(title, forKey: "title")
         }
-        
-        if let link = self.link
-        {
-            aCoder.encodeObject(link, forKey: "link")
-        }
-        
+
+        //        if let link = self.link
+        //        {
+        //            aCoder.encodeObject(link, forKey: "link")
+        //        }
+
         if let guid = self.guid
         {
             aCoder.encodeObject(guid, forKey: "guid")
         }
-        
+
         if let pubDate = self.pubDate
         {
             aCoder.encodeObject(pubDate, forKey: "pubDate")
         }
-        
+
         if let itemDescription = self.itemDescription
         {
             aCoder.encodeObject(itemDescription, forKey: "description")
         }
-        
+
         if let content = self.content
         {
             aCoder.encodeObject(content, forKey: "content")
         }
-        
+
         if let commentsLink = self.commentsLink
         {
             aCoder.encodeObject(commentsLink, forKey: "commentsLink")
         }
-        
+
         if let commentsCount = self.commentsCount
         {
             aCoder.encodeObject(commentsCount, forKey: "commentsCount")
         }
-        
+
         if let commentRSSLink = self.commentRSSLink
         {
             aCoder.encodeObject(commentRSSLink, forKey: "commentRSSLink")
         }
-        
+
         if let author = self.author
         {
             aCoder.encodeObject(author, forKey: "author")
         }
-        
+
         aCoder.encodeObject(categories, forKey: "categories")
     }
 }
--- SwiftRSS/RSSParser.swift
+++ SwiftRSS/RSSParser.swift
@@ -1,50 +1,53 @@
-//
-//  RSSParser.swift
-//  SwiftRSS_Example
-//
-//  Created by Thibaut LE LEVIER on 05/09/2014.
-//  Copyright (c) 2014 Thibaut LE LEVIER. All rights reserved.
-//
 
 import UIKit
 
+// TODO [DAC] this mixes feed and feed item tags, they should be separated.
+enum NodeElement : String {
+    case Item = "item"
+    case Title = "title"
+    case Link = "link"
+    case GUID = "guid"
+    case PublicationDate = "pubDate"
+    case Description = "description"
+    case ContentType = "content:encoded"
+    case Language = "language"
+    case Property = "rdctv:property"
+    case ParallaxImage = "rdctv:image"
+    case LastBuildDate = "lastBuildDate"
+    case Generator = "generator"
+    case Copyright = "copyright"
+    // Specific to Wordpress:
+    case CommentsLink = "comments"
+    case CommentsCount = "slash:comments"
+    case CommentRSSLink = "wfw:commentsRSS"
+    case Author = "dc:creator"
+    case Category = "category"
+}
+extension String {
+    func rssNodeElement() -> NodeElement? {
+        return NodeElement(rawValue: self)
+    }
+}
+
 class RSSParser: NSObject, NSXMLParserDelegate {
-    
+
     class func parseFeedForRequest(request: NSURLRequest, callback: (feed: RSSFeed?, error: NSError?) -> Void)
     {
         let rssParser: RSSParser = RSSParser()
-        
+
         rssParser.parseFeedForRequest(request, callback: callback)
     }
-    
+
+    static var sharedURLSession : NSURLSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+
     var callbackClosure: ((feed: RSSFeed?, error: NSError?) -> Void)?
     var currentElement: String = ""
     var currentItem: RSSItem?
     var feed: RSSFeed = RSSFeed()
-    
-    // node names
-    let node_item: String = "item"
-    
-    let node_title: String = "title"
-    let node_link: String = "link"
-    let node_guid: String = "guid"
-    let node_publicationDate: String = "pubDate"
-    let node_description: String = "description"
-    let node_content: String = "content:encoded"
-    let node_language: String = "language"
-    let node_lastBuildDate = "lastBuildDate"
-    let node_generator = "generator"
-    let node_copyright = "copyright"
-    // wordpress specifics
-    let node_commentsLink = "comments"
-    let node_commentsCount = "slash:comments"
-    let node_commentRSSLink = "wfw:commentRss"
-    let node_author = "dc:creator"
-    let node_category = "category"
-    
+
     func parseFeedForRequest(request: NSURLRequest, callback: (feed: RSSFeed?, error: NSError?) -> Void)
     {
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+        let theTask = RSSParser.sharedURLSession.dataTaskWithRequest(request) { (data, response, error) -> Void in
 
             if ((error) != nil)
             {
@@ -53,20 +56,21 @@ class RSSParser: NSObject, NSXMLParserDelegate {
             else
             {
                 self.callbackClosure = callback
-                
+
                 let parser : NSXMLParser = NSXMLParser(data: data!)
                 parser.delegate = self
                 parser.shouldResolveExternalEntities = false
                 parser.parse()
             }
         }
+        theTask.resume()
     }
-    
-// MARK: NSXMLParserDelegate
+
+    // MARK: NSXMLParserDelegate
     func parserDidStartDocument(parser: NSXMLParser)
     {
     }
-    
+
     func parserDidEndDocument(parser: NSXMLParser)
     {
         if let closure = self.callbackClosure
@@ -74,124 +78,105 @@ class RSSParser: NSObject, NSXMLParserDelegate {
             closure(feed: self.feed, error: nil)
         }
     }
-    
+
     func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
-        
-        if elementName == node_item
+
+        if let e = elementName.rssNodeElement() where e == .Item
         {
             self.currentItem = RSSItem()
         }
-        
+
         self.currentElement = ""
-        
+
     }
-   
+
     func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        
-        if elementName == node_item
+        // Need to have an element that we know how to handle
+        guard let element = elementName.rssNodeElement() else { return }
+
+        if let e = elementName.rssNodeElement() where e == .Item
         {
             if let item = self.currentItem
             {
                 self.feed.items.append(item)
             }
-            
+
             self.currentItem = nil
             return
         }
-        
+
         if let item = self.currentItem
         {
-            if elementName == node_title
-            {
+            switch element {
+
+            case .Title :
                 item.title = self.currentElement
-            }
-            
-            if elementName == node_link
-            {
-                item.setLink1(self.currentElement)
-            }
-            
-            if elementName == node_guid
-            {
+
+            case .Link:
+                item.addLink(self.currentElement)
+
+            case .Property:
+                item.addPropertyID(self.currentElement)
+
+            case .GUID:
                 item.guid = self.currentElement
-            }
-            
-            if elementName == node_publicationDate
-            {
+
+            case .PublicationDate:
                 item.setPubDate1(self.currentElement)
-            }
-            
-            if elementName == node_description
-            {
+
+            case .Description:
                 item.itemDescription = self.currentElement
-            }
-            
-            if elementName == node_content
-            {
+
+            case .ContentType:
                 item.content = self.currentElement
-            }
-            
-            if elementName == node_commentsLink
-            {
+
+            case .CommentsLink:
                 item.setCommentsLink1(self.currentElement)
-            }
-            
-            if elementName == node_commentsCount
-            {
+
+            case .CommentsCount:
                 item.commentsCount = Int(self.currentElement)
-            }
-            
-            if elementName == node_commentRSSLink
-            {
+
+            case .CommentRSSLink:
                 item.setCommentRSSLink1(self.currentElement)
-            }
-            
-            if elementName == node_author
-            {
+
+            case .Author:
                 item.author = self.currentElement
-            }
-            
-            if elementName == node_category
-            {
+
+            case .Category:
                 item.categories.append(self.currentElement)
+
+            default:
+                print("Unmatched case: \(element)")
             }
-            
         }
         else
         {
-            if elementName == node_title
-            {
+            switch element {
+
+            case .Title :
                 feed.title = self.currentElement
-            }
-            
-            if elementName == node_link
-            {
+
+            case .Link:
                 feed.setLink1(self.currentElement)
-            }
-            
-            if elementName == node_description
-            {
-                feed.feedDescription = self.currentElement
-            }
-            
-            if elementName == node_language
-            {
+
+            case .Language:
                 feed.language = self.currentElement
-            }
-            
-            if elementName == node_lastBuildDate
-            {
+
+            case .LastBuildDate:
                 feed.setlastBuildDate(self.currentElement)
-            }
-            
-            if elementName == node_generator
-            {
+
+            case .Description:
+                feed.feedDescription = self.currentElement
+
+            case .Generator:
                 feed.generator = self.currentElement
-            }
-            
-            if elementName == node_copyright
-            {
+
+            case .Copyright:
                 feed.copyright = self.currentElement
+
+            default:
+                print("Unmatched case: \(element)")
+
             }
         }
     }
@@ -207,5 +192,5 @@ class RSSParser: NSObject, NSXMLParserDelegate {
             closure(feed: nil, error: parseError)
         }
     }
-
+    
 }
